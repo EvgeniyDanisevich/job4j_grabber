75bb87174cd575a3cf9f732bf20f843a3bcb880a
EvgeniyDanisevich <a937va@gmail.com>
2021-02-14 15:42:57 +0300

    1. Quartz [#451521]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -3,9 +3,8 @@ package ru.job4j.quartz;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.BufferedReader;
 import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Properties;
 
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
@@ -13,32 +12,24 @@ import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        int interval = 0;
         try (InputStream is = AlertRabbit.class
                 .getClassLoader().getResourceAsStream("rabbit.properties")) {
-            if (is == null) {
-                throw new IllegalArgumentException("File not found");
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            String[] strings = reader.readLine().split("=");
-            interval = Integer.parseInt(strings[1]);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        try {
+            Properties properties = new Properties();
+            properties.load(is);
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(interval)
+                    .withIntervalInSeconds(
+                            Integer.parseInt(properties.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(times)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException se) {
-            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
