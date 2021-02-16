bf56a0a798bfcff58f26954a229d996d5ed757d5
EvgeniyDanisevich <a937va@gmail.com>
2021-02-16 19:08:07 +0300

    2.3. Загрузка деталей поста. [#451529]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,9 +5,12 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.time.LocalDateTime;
+
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < 2; i++) {
             Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + i).get();
             Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
@@ -16,7 +19,19 @@ public class SqlRuParse {
                 System.out.println(href.text());
                 Element data = td.parent().child(5);
                 System.out.println(Parsing.dateParser(data.text()));
+                System.out.println(details(href.attr("href")));
+                System.out.println(date(href.attr("href")));
             }
         }
+    }
+
+    public static String details(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        return doc.select(".msgBody").get(1).text();
+    }
+
+    public static LocalDateTime date(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        return Parsing.dateParser(doc.select(".msgFooter").get(0).text().split(" \\[")[0]);
     }
 }
