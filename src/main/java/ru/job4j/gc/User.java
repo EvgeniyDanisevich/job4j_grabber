30323378183f090aaca9bd49148861e2133e4958
EvgeniyDanisevich <a937va@gmail.com>
2021-03-08 20:46:22 +0300

    Revert "1. Принципы Kiss, Dry и Yagni [#451635]"
    This reverts commit d7f9ae8b

--- src/main/java/ru/job4j/gc/User.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.gc;
-
-public class User {
-    @Override
-    public void finalize() throws Throwable {
-
-    }
-}
--- src/main/java/ru/job4j/template/Generator.java
+++
@@ -1,2 +0,0 @@
-package ru.job4j.template;public class Generator {
-}
